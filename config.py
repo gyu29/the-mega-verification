d92910f1dd1be4b66cae9e4e0d713b3f999b18f1
Gene Yu <112839874+gyu29@users.noreply.github.com>
2024-05-13 13:22:38 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ class Config:
     role_name = "member"
     message_id = "1239364978633605200"
     prefix = "."
-    bot_token = "MTIzOTIxNjA2Njg1NzY2ODYzMA.GPVVzG.5Cpn2CImBwFAT58y1xCecD_y_CwbbyHcRnOAiY"
+    bot_token = "MTIzOTIxNjA2Njg1NzY2ODYzMA.Gnrsu2.wA3Ido4f4x0j0UTzxBvt5cmx269d2MXBYwq9w0"
     channel_id = "1239215987287658557"
