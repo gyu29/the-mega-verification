7390f79007a46c54ac0bab777c0e172d4af198df
Gene Yu <112839874+gyu29@users.noreply.github.com>
2024-05-13 13:13:42 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    role_name = "member"
+    message_id = "1239364978633605200"
+    prefix = "."
+    bot_token = "MTIzOTIxNjA2Njg1NzY2ODYzMA.GPVVzG.5Cpn2CImBwFAT58y1xCecD_y_CwbbyHcRnOAiY"
+    channel_id = "1239215987287658557"
